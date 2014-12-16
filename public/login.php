25a2f3696717d4eeea720a22b7e2818cc03d19bc
Sibar Al-ani <sibar.alani@gmail.com>
2014-12-16 11:44:29 +0100

    login file

---
+++ public/login.php
@@ -0,0 +1,22 @@
+<?php
+
+
+?>
+
+<h1>Login!</h1>
+<table>
+	<form action="login.php" method="POST">
+	
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="spara" value="login"></td>
+		</tr>
+	</form>
+</table>
