1a5302aafa00f923e9c8663e0402f28c5c5cfd01
Sibar Al-ani <sibar.alani@gmail.com>
2014-12-16 09:33:18 +0100

    form code

--- public/signup.php
+++ public/signup.php
@@ -1,2 +1,37 @@
+
+
+
 <?php
+	if(isset($_POST['spara'])){
+	
+		echo "tjaba";
+	
+	}
 ?>
+<h1>Sign Up!</h1>
+<table>
+	<form action="signup.php" method="POST">
+		<tr>
+			<td>username:</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>email:</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td>Re-password:</td>
+			<td><input type="password" name="rePass"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="spara" value="signup"></td>
+		</tr>
+		
+	</form>
+</table>
+
+
